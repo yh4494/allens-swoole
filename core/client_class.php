4c303cea2dcf1203848961a068d86b74a192ac4c
喻越 <yuy@xinguangnet.com>
2017-06-01 17:48:07 +0800

    main frame

--- core/client_class.php
+++ core/client_class.php
@@ -10,21 +10,23 @@ class client_class{
         $this->client = new \swoole_client(SWOOLE_SOCK_TCP);
     }
     public function connect() {
-        $this->client->on('connect', function ($cli){
-            echo '客户端连接已经创建';
-        });
+        $this->client->on('connect', [$this, 'deal_connect']);
 
-        $this->client->on("close", function($cli){
-            $cli->close(); // 1.6.10+ 不需要
-            echo "close\n";
-        });
-
-        $this->client->on("error", function($cli){
-            exit("error\n");
-        });
+//        $this->client->on("close", function($cli){
+//            $cli->close(); // 1.6.10+ 不需要
+//            echo "close\n";
+//        });
+//
+//        $this->client->on("error", function($cli){
+//            exit("error\n");
+//        });
 
         if( !$this->client->connect("127.0.0.1", 9501 , 1) ) {
             echo "Error: {$this->client->errMsg}[{$this->client->errCode}]\n";
         }
+    }
+
+    function deal_connect($cli){
+        echo '客户端连接已经创建';
     }
 }
