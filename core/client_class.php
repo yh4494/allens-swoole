cb2d9f8c506193578743bddb980822177a036665
喻越 <yuy@xinguangnet.com>
2017-06-01 17:50:21 +0800

    main frame

--- core/client_class.php
+++ core/client_class.php
@@ -10,17 +10,6 @@ class client_class{
         $this->client = new \swoole_client(SWOOLE_SOCK_TCP);
     }
     public function connect() {
-        $this->client->on('connect', [$this, 'deal_connect']);
-
-//        $this->client->on("close", function($cli){
-//            $cli->close(); // 1.6.10+ 不需要
-//            echo "close\n";
-//        });
-//
-//        $this->client->on("error", function($cli){
-//            exit("error\n");
-//        });
-
         if( !$this->client->connect("127.0.0.1", 9501 , 1) ) {
             echo "Error: {$this->client->errMsg}[{$this->client->errCode}]\n";
         }
